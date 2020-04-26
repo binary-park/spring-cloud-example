22fc73eab6b9008e0e34aecee170fce8afb80680
one.0 <wonyoung.park@kurlycorp.com>
2020-04-26 18:26:33 +0900

    first commit

---
+++ src/main/java/io/github/wyparks2/springcloudexample/SpringCloudExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.wyparks2.springcloudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/github/wyparks2/springcloudexample/SpringCloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wyparks2.springcloudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
